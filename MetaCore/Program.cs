694d90b4552e34b808a606c4c9d446c187054c0e
Dheeraj Jain <dheeraj.jain@thalesgroup.com>
2021-03-24 00:19:09 +0530

    missing dll issue

---
+++ MetaCore/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeakDetection;
+
+namespace MetaNumericsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LeakDetectionOperations ldo = new LeakDetectionOperations();
+            ldo.leakCheck(1.1f, 2.1f, 3.1f, 5.1f, 6.1f);
+        }
+    }
+}
---
+++ MetaNumerics/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using Meta.Numerics;
+using Meta.Numerics.Matrices;
+using Meta.Numerics.Statistics;
+using Meta.Numerics.Statistics.Distributions;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LeakDetection
+{
+
+    public class LeakDetectionOperations
+    {
+
+        private int correlationRange;
+        private bool leakDetected;
+
+        Queue<float> ch4Que;
+        Queue<float> c2h6Que;
+
+        public LeakDetectionOperations(int co = 24)
+        {
+
+            Console.WriteLine("Leak Detection Operations Constructor");
+        }
+
+        public int leakCheck(float time, float ch4, float c2h6, float latitude, float longitude)
+        {
+
+            Console.WriteLine("\n\tLeak Check");
+
+            double result = ComplexMath.Abs(10);
+
+            return 1;
+        }
+    }
+}
